5818d43d4505f234e2a50687f7323ec8e14901ff
zoeo8159 <114589925+zoeo8159@users.noreply.github.com>
2022-09-28 09:33:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "zoe"; // update the part BETWEEN the "quotes"
+let userName = "zoeo859"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
